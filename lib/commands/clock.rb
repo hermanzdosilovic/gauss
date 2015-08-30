786bddd46ef59762795bdb227f0767af2890e5d0
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2015-08-30 12:47:25 +0200

    Cleaned clock command

--- lib/commands/clock.rb
+++ lib/commands/clock.rb
@@ -4,14 +4,22 @@ require 'git'
 module Gauss
   module Clock
     def self.run(args_ignorable = [])
-      git = Git.open(`git rev-parse --show-toplevel`.strip)
+      git = Git.open(`git rev-parse --show-toplevel 2> /dev/null`.strip)
 
       clock = ''
       git.log.since('1 day ago').each do |commit|
         clock << "* #{commit.message}\n"
       end
 
-      Clipboard.copy clock
+      if clock.empty?
+        puts 'You do not have any commits from today.'
+      else
+        puts 'Your commits for today are now copied in clipboard:'
+        puts clock
+        Clipboard.copy clock
+      end
+    rescue
+      puts 'gauss: Not a git repository (or any of the parent directories): .git'
     end
   end
 end
--- lib/commands/init.rb
+++ lib/commands/init.rb
@@ -6,29 +6,27 @@ module Gauss
     AUTHENTICATION_SESSION = '/users/login'
 
     def self.run(args_ignorable = [])
-      begin
-        print 'username: '
-        username = STDIN.gets.chomp
+      print 'username: '
+      username = STDIN.gets.chomp
 
-        print 'password: '
-        password = STDIN.noecho(&:gets).chomp
-        puts
+      print 'password: '
+      password = STDIN.noecho(&:gets).chomp
+      puts
 
-        params = { query: { username: username, password: password } }
-        response = HTTParty.post(BASE_URL + AUTHENTICATION_SESSION, params)
+      params = { query: { username: username, password: password } }
+      response = HTTParty.post(BASE_URL + AUTHENTICATION_SESSION, params)
 
-        if response.code != 200
-          puts "Invalid username or password."
-          return
-        end
-
-        puts "Authentication successfull"
-        gauss_key_file = File.open(GAUSS_KEY, 'w')
-        gauss_key_file.puts("PRODUCTIVE_API_KEY=#{response['response']['token']}")
-        gauss_key_file.close
-      rescue Interrupt
-        puts
+      if response.code != 200
+        puts "Invalid username or password."
+        return
       end
+
+      puts "Authentication successfull"
+      gauss_key_file = File.open(GAUSS_KEY, 'w')
+      gauss_key_file.puts("PRODUCTIVE_API_KEY=#{response['response']['token']}")
+      gauss_key_file.close
+    rescue Interrupt
+      puts
     end
   end
 end
